ddbdc9ddc17949c201f719a7a9a0cc66f55d5808
Khushal joshi <160943342+khushal728@users.noreply.github.com>
2025-04-11 13:03:41 +0530

    Update WebScrapper.py

--- WebScrapper.py
+++ WebScrapper.py
@@ -13,13 +13,6 @@ import spacy
 import subprocess
 import sys
 
-# Automatically download the model if not present
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
-    nlp = spacy.load("en_core_web_sm")
-
 # Load NLP model
 nlp = spacy.load("en_core_web_sm")
 
